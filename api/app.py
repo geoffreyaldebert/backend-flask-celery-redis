bf4216b8f860a86069d5e0ef2da8e0bf9df58948
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2020-06-16 15:24:46 +0200

    Init

---
+++ api/app.py
@@ -0,0 +1,75 @@
+from flask import Flask, jsonify, request
+from flask import url_for
+from worker import celery
+import celery.states as states
+
+app = Flask(__name__)
+
+
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+@app.route('/add/<int:param1>/<int:param2>')
+def add(param1: int, param2: int) -> str:
+    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
+    response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+    return response
+
+
+@app.route('/check/<string:task_id>')
+def check_task(task_id: str) -> str:
+    res = celery.AsyncResult(task_id)
+    if res.state == states.PENDING:
+        return res.state
+    else:
+        return str(res.result)
+
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+    
+@app.route('/books3', methods=['GET', 'POST'])
+def all_books2():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0', port=5001)
